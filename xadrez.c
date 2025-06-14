68cf2b4a543fdb127fe9fa8430bd13888b469e37
Adriano0002 <adriano.giacomini29@gmail.com>
2025-06-14 02:10:32 +0000

    Movimento Rainha e ajustes

--- xadrez.c
+++ xadrez.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
-
+    // Simulação de Movimento de Peças de Xadrez usando for, while e do-while
+    
 int main() {
     
     int casasTorre = 5; 
-     // Simulação da Torre (movimento para a direita) - usando FOR
+     // Movimentaçao da Torre (movimento para a direita)
     printf("Movimento da Torre:\n");
     for (int i = 0; i < casasTorre; i++) {
         printf("Direita\n");
     }
      int casasBispo = 5;
-     // Simulação do Bispo (diagonal para cima e à direita) - usando WHILE
+
+     // Movimentaçao do Bispo (diagonal para cima e à direita) 
       printf("\nMovimento do Bispo:\n");
     int i = 0;
     while (i < casasBispo) {
         printf("Cima Direita\n");
         i++;
     }
+    int casasRainha=8;
 
+     // Movimentaçao da Rainha (movimento a esquerda)
+      printf("\nMovimento da Rainha:\n");
+    int j = 0;
+    do {
+        printf("Esquerda\n");
+        j++;
+    } while (j < casasRainha);
 
 
 }
