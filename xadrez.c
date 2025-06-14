b8bd5ee7ff01aa79fd06d6e897f13984dcac1a35
Adriano0002 <adriano.giacomini29@gmail.com>
2025-06-14 02:02:25 +0000

    Movimentaçao do Bispo

--- xadrez.c
+++ xadrez.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int casasTorre = 5;
- // Simulação da Torre (movimento para a direita) - usando FOR
+    
+    int casasTorre = 5; 
+     // Simulação da Torre (movimento para a direita) - usando FOR
     printf("Movimento da Torre:\n");
     for (int i = 0; i < casasTorre; i++) {
         printf("Direita\n");
     }
-
-
+     int casasBispo = 5;
+     // Simulação do Bispo (diagonal para cima e à direita) - usando WHILE
+      printf("\nMovimento do Bispo:\n");
+    int i = 0;
+    while (i < casasBispo) {
+        printf("Cima Direita\n");
+        i++;
+    }
 
 
 
